70a8dc716a33708430aab3627c83b04d2871e3e6
Daniel <dhidalgosanchez96@gmail.com>
2018-04-02 17:25:49 +0200

    Second Commit

--- SerpentTest.py
+++ SerpentTest.py
@@ -1,12 +1,29 @@
 from CryptoPlus.Cipher import python_Serpent
+import time
 
 key = '000102030405060708090A0B0C0D0E0F'.decode('hex')
 IV = '00000000000000000000000000000000'.decode('hex')
-plaintext = ('33B3DC87EDDD9B0F6A1F407D14919365'*3).decode('hex')
+plaintext = ('33B3DC87EDDD9B0F6A1F407D14919365'*32*1000).decode('hex')
+
+cipher_init_time = time.time()
+
 cipher = python_Serpent.new(key, python_Serpent.MODE_CBC, IV)
 cipher_text = cipher.encrypt(plaintext)
+
+cipher_finish_time = time.time()
+
+cipher_time = cipher_finish_time - cipher_init_time
+
+print '\nTiempo de cifrado -> ' + str(cipher_time)
+
+decipher_init_time = time.time()
+
 decipher = python_Serpent.new(key, python_Serpent.MODE_CBC, IV)
 decipher_text = decipher.decrypt(cipher_text).encode('hex').upper()
 
-print cipher_text
-print decipher_text
+decipher_finish_time = time.time()
+
+decipher_time = decipher_finish_time - decipher_init_time
+
+
+print 'Tiempo de descifrado -> ' + str(decipher_time)
